19393b80bc2fd8a4527d2c0cf07b3b36c0e380c6
KENKUN-1031 <saryusoul@Kenseis-MacBook-Pro.local>
2024-03-15 21:50:13 +0900

    bg-switcher-bug

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function($){
+    $('.bg-switcher').bgSwitcher({
+        images:["/images/topPic/zoeTop.png","/images/topPic/blueBottleTop.png", "/images/topPic/coq1fTop.png", "/images/topPic/keshipearlTop.png"],
+        interval: 5000,
+        loop: true,
+    });
+    console.log("test");
+});
