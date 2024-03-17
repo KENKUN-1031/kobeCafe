cdb48ac258fbd9f24a4bb03cca07461fb8d073c4
KENKUN-1031 <saryusoul@Kenseis-MacBook-Pro.local>
2024-03-17 09:48:33 +0900

    final

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(function($){
-    $('.bg-switcher').bgSwitcher({
-        images:["/images/topPic/zoeTop.png","/images/topPic/blueBottleTop.png", "/images/topPic/coq1fTop.png", "/images/topPic/keshipearlTop.png"],
-        interval: 5000,
-        loop: true,
-    });
-    console.log("test");
-});
+// $(function($){
+//     $('.bg-switcher').bgSwitcher({
+//         images:["/images/topPic/zoeTop.png","/images/topPic/blueBottleTop.png", "/images/topPic/coq1fTop.png", "/images/topPic/keshipearlTop.png"],
+//         interval: 5000,
+//         loop: true,
+//     });
+//     console.log("test");
+// });
